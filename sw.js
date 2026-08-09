fa5921eca4cd07315bfb0dceea5d33a4d9230ef7
Abdulrahman <azhryb824@gmail.com>
2026-08-09 20:45:41 +0300

    fin9: include all visit statuses (incl. cancelled) consistently in monthly visits PDF report

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shumoos-v20260809-fin8";
+const CACHE = "shumoos-v20260809-fin9";
 const ASSETS = [
   "/", "/index.html", "/login.html", "/register.html", "/dashboard.html",
   "/styles.css", "/app.js", "/manifest.json", "/pdfmake-gen.js",
