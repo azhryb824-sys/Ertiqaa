c5b29f9840c44d9cd7c4217b1e7d4427f7062884
Abdulrahman <azhryb824@gmail.com>
2026-08-09 14:11:51 +0300

    إصلاح ظهور تبويبات الملف المالي: display:block صريح في cfSectionStyle والنقر (إصدار 4)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shumoos-v20260809-fin3";
+const CACHE = "shumoos-v20260809-fin4";
 const ASSETS = [
   "/", "/index.html", "/login.html", "/register.html", "/dashboard.html",
   "/styles.css", "/app.js", "/manifest.json", "/pdfmake-gen.js",
