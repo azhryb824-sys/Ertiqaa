ab556434247781688c7afe298928d870489aab83
Abdulrahman <azhryb824@gmail.com>
2026-08-09 14:44:26 +0300

    إصدار 5: إصلاح تبويبات الإدارة المالية العامة والملف المالي للمورد والموظف (display:block)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shumoos-v20260809-fin4";
+const CACHE = "shumoos-v20260809-fin5";
 const ASSETS = [
   "/", "/index.html", "/login.html", "/register.html", "/dashboard.html",
   "/styles.css", "/app.js", "/manifest.json", "/pdfmake-gen.js",
