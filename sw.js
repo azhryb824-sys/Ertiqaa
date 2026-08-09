cfb9e912f4c6a040c0f254c7ee77cd0e8a0405f3
Abdulrahman <azhryb824@gmail.com>
2026-08-09 13:13:19 +0300

    إصدار 3: شارة بناء ظاهرة بالرأس + كاش جديد

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shumoos-v20260809-fin2";
+const CACHE = "shumoos-v20260809-fin3";
 const ASSETS = [
   "/", "/index.html", "/login.html", "/register.html", "/dashboard.html",
   "/styles.css", "/app.js", "/manifest.json", "/pdfmake-gen.js",
